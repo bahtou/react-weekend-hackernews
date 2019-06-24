3852c978c7f7db21f8e70a267f6ea87cf56bea43
kdybvig <kdybvig@Keiths-MacBook-Pro.local>
2019-06-24 15:23:24 -0500

    score top domains and track error and loading status

--- src/pages/TopDomains/index.js
+++ src/pages/TopDomains/index.js
@@ -9,50 +9,96 @@ async function fetchStory(articleId) {
 }
 
 async function fetchStories() {
-  const response = await fetch('http://hacker-news.firebaseio.com/v0/topstories.json?print=pretty', { method: 'GET', mode: 'cors' });
-  return await response.json();
+  try {
+    const response = await fetch('http://hacker-news.firebaseio.com/v0/topstories.json?print=pretty', { method: 'GET', mode: 'cors' });
+    return await response.json();
+  } catch(error) {
+    return {error}
+  }
+}
+
+const getTopDomainsFromStories = (stories) => {
+  const domains = stories.reduce((initDomains, story) => {
+    const newDomains = initDomains.slice()
+    if (!story.url) return newDomains
+    const url = new URL(story.url);
+    const urlIndex = newDomains.findIndex(domainObj => domainObj.url === url.hostname)
+    if(urlIndex === -1) {
+      newDomains.push({url: url.hostname, score: story.score, numPosts: 1})
+    } else {
+      newDomains[urlIndex].score += story.score;
+      newDomains[urlIndex].numPosts ++
+    }
+    return newDomains
+  }, []);
+  domains.sort((first, second) => {
+    return second.score - first.score
+  })
+  //Only display the top 20 domains
+  return domains.slice(0,20)
 }
 
 async function fetchTopDomains() {
+  let isLoading = true
+  let isError = false
+  let domains = []
   const _stories = await fetchStories();
 
-  const storyIds = _stories.filter((value, index, arr) => index < 50);
+  if(_stories.error) {
+    return {isLoading: false, isError: true, domains: []}
+  }
+
+  const storyIds = _stories.slice();
   const storyPromises = storyIds.map(async storyId => await fetchStory(storyId));
   const stories = await Promise.all(storyPromises);
-
-  const domains = stories.map(story => {
-    if (story.url) {
-      const url = new URL(story.url);
-      return url.hostname;
-    }
-  });
-
-  return domains;
+  if(stories.some(story => story.error)) {
+    isError = true;
+  } else {
+    domains = getTopDomainsFromStories(stories)
+  }
+  return {isLoading, isError, domains}
 }
 
 const useFetchTopDomains = () => {
   const [ domains, setDomains ] = useState([]);
-
+  const [ isLoading, setIsLoading] = useState(true)
   useEffect(() => {
     const fetchData = async () => {
       const results = await fetchTopDomains();
-      setDomains(results);
+      setDomains(results.domains);
+      setIsLoading(false)
     };
 
     fetchData();
   }, []);
 
-  return domains;
+  return {isLoading, domains}
 };
 
 function TopDomains() {
-  const domains = useFetchTopDomains();
+  const {isLoading, domains} = useFetchTopDomains();
 
-  return (
-    <ul>
-      { domains.map((domain, index) => <li key={ index }>{ domain }</li> )}
-    </ul>
-  );
+  return  isError
+  ? (<div style={{color: 'red'}}>Something went wrong...</div>)
+  : (isLoading
+  ? <div>...Loading...</div>
+  : (
+    <table>
+      <tbody>
+        <tr>
+          <th>Domain</th>
+          <th>Score</th>
+          <th># Posts</th>
+        </tr>
+      { domains.map((domain, index) => (
+        <tr key={ domain.url}>
+            <td>{domain.url}</td>
+            <td>{domain.score}</td>  
+            <td>{domain.numPosts}</td>
+        </tr> ))}
+      </tbody>
+    </table>
+  ));
 }
 
 
