2c238a23608b0435699db31cef6f3abf279f7feb
kdybvig <kdybvig@Keiths-MacBook-Pro.local>
2019-06-25 12:57:40 -0500

    stylized search page and added information

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useReducer } from 'react';
-
+import moment from 'moment';
+import styles from './styles.css'
 
 const dataFetchReducer = (state, action) => {
   switch (action.type) {
@@ -41,7 +42,7 @@ const useDataAPI = (initUrl, initData) => {
       try {
         const response = await fetch(url, { method: 'GET', mode: 'cors' });
         const results = await response.json();
-
+        console.log(results)
         // setData({ hits: results.hits });
         dispatch({ type: 'FETCH_SUCCESS', payload: results.hits });
       } catch (err) {
@@ -69,23 +70,44 @@ function Search() {
 
   return (
     <>
-      <input
-        type="text"
-        value={ query }
-        onChange={ event => setQuery(event.target.value) } />
-      <button type="button" onClick={ () => doFetch(`http://hn.algolia.com/api/v1/search?query=${query}`) }>
-        Search
-      </button>
-
       {isError && <div>Something went wrong ...</div>}
 
       {isLoading
         ? (<div>Loading ...</div>)
-        : (<ul>
-          {data.hits.map(item => (
-            <li key={ item.objectID }>
-              <a href={ item.url }>{item.title}</a>
-            </li>))}
+        : (<ul className={styles.stories}>
+            <li className={ styles.listTop }>
+              <h2 className={ styles.searchTitle }>Search Hackner News</h2>
+              <input
+                className={ styles.searchInput }
+                type="text"
+                value={ query }
+                onChange={ event => setQuery(event.target.value) } />
+              <button className={ styles.searchButton } type="button" onClick={ () => doFetch(`http://hn.algolia.com/api/v1/search?query=${query}`) }>
+                Search
+              </button>
+            </li>
+          {data.hits.map(item => {
+            
+            const url = new URL (item.url)
+            const timeAgo = moment(item.created_at).fromNow()
+            const userLink = `https://news.ycombinator.com/user?id=${item.author}`
+            const commentLink = `https://news.ycombinator.com/item?id=${item.objectID}`
+           
+            return (
+              <li key={ item.objectID } className={ styles.story }>
+                <div>
+                  <a className={styles.title} href={ item.url }>{ item.title } </a>
+                </div>
+                <div>
+                  <span> { item.points } points </span>
+                  <span> | <a href={ userLink }>{ item.author }</a> </span>
+                  <span> | { timeAgo } </span>
+                  <span> | <a href={ commentLink }>{ item.num_comments } comments</a> </span>
+                  <span> | <a href={ 'http://' + url.hostname }>{ url.hostname }</a> </span>
+                </div>  
+              </li>
+            )
+          })}
         </ul>)
       }
     </>
