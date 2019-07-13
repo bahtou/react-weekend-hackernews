055a73fee740267a69b4bb4c3970baf29c77832f
Jeeth Suresh <jeethsuresh@Jeeths-MacBook-Air.local>
2019-07-13 12:50:26 -0500

    Link logo to homepage and fix CSS to stay consistent

--- src/components/ReactWeekend/index.js
+++ src/components/ReactWeekend/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import webFountainLogo from 'Images/logo.jpg';
 import HX from 'Elements/HX';
-import { title } from './styles.css';
+import { title, linkTitle } from './styles.css';
 
 
 const styles = {
@@ -10,14 +10,17 @@ const styles = {
   color: '#0B69AF'
 };
 
+
 function ReactWeekend() {
   return (
     <>
+    <a href="/" className={linkTitle}>
       <img src={webFountainLogo} alt="web-fountain-logo" width="35px" height="39px" />
       <div className={title}>
         <HX hx={'h2'} style={styles}>React</HX>
         <HX hx={'h2'} style={styles}>Weekend</HX>
       </div>
+    </a>
     </>
   );
 }
