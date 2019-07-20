09ca07f1d61401360435ed087cc1c6a8fe12c17b
bahtou <bahtou@users.noreply.github.com>
2019-07-20 07:35:09 -0500

    refactor: Home Page
    * use a hook to remove fetch logic from component
    * TODO: handle loading & error states

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,65 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import { useHNstories } from 'Hooks';
+import { TOP_STORIES } from 'Endpoints';
+
 import { MainGridLayout, NavGrid, HeaderGrid, ContentGrid } from 'Layouts';
 import Story from 'Components/Story';
 import TopHeader from 'Components/TopHeader';
 import HX from 'Elements/HX';
 
 
-async function fetchStory(articleId) {
-  try {
-    const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${articleId}.json?print=pretty`, { method: 'GET', mode: 'cors' });
-    const results = await response.json();
-    return results;
-  } catch (error) {
-    return { error };
-  }
-}
-
-async function fetchStories() {
-  try {
-    const response = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json', { method: 'GET', mode: 'cors' });
-    const results = await response.json();
-    return results;
-  } catch (error) {
-    return { error };
-  }
-}
-
-async function fetchTopStories(limit) {
-  const _stories = await fetchStories();
-
-  if (_stories.error) {
-    return { isLoading: false, isError: true, stories: [] };
-  }
-
-  const storyIds = _stories.slice(0, limit);
-  const storyPromises = storyIds.map(storyId => fetchStory(storyId));
-  const stories = await Promise.all(storyPromises);
-
-  if (stories.some(story => story.error)) {
-    return { isLoading: false, isError: true, stories: [] };
-  }
-  return { isLoading: false, isError: false, stories };
-}
-
 const numStories = 20;
 function Home({ history }) {
-  const [stories, setStories] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const results = await fetchTopStories(numStories);
-      setStories(results.stories);
-      setIsLoading(results.isLoading);
-      setIsError(results.isError);
-    };
-
-    fetchData();
-  }, []);
+  const { stories } = useHNstories(TOP_STORIES, numStories);
 
   return (
     <MainGridLayout>
@@ -76,15 +29,9 @@ function Home({ history }) {
       </NavGrid>
 
       <ContentGrid>
-        {isError
-          ? <div>Something went wrong...</div>
-          : isLoading
-            ? <div>...Loading...</div>
-            : (
-              <ul>
-                {stories.map((story, index) => <Story key={story.id} index={index} story={story} history={history} />)}
-              </ul>
-            )
+        <ul>
+          {stories.map((story, index) => <Story key={story.id} index={index} story={story} history={history} />)}
+        </ul>
       }
       </ContentGrid>
     </MainGridLayout>
